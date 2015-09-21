5c02c76124e8bc9cf4ac67543aed62f3c780436e
JiangChu <main1008@163.com>
2015-09-21 21:53:40 +0800

    实现TabHost控件

--- WeightScale/app/src/main/java/com/pomelo/weightscale/MainActivity.java
+++ WeightScale/app/src/main/java/com/pomelo/weightscale/MainActivity.java
@@ -1,16 +1,53 @@
 package com.pomelo.weightscale;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RadioGroup;
+import android.widget.TabHost;
+import android.app.TabActivity;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends TabActivity implements RadioGroup.OnCheckedChangeListener {
+
+    private static final String TAB_TAG_CHART  = "iChart";
+    private static final String TAB_TAG_DATAS  = "iDatas";
+    private static final String TAB_TAG_SQUARE = "iSquare";
+    private static final String TAB_TAG_MORE   = "iMore";
+
+    private TabHost mTabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getActionBar().setDisplayHomeAsUpEnabled(false);
+        getActionBar().setBackgroundDrawable(getResources().getDrawable(R.drawable.actionbar_bg));
         setContentView(R.layout.activity_main);
+
+        RadioGroup mMainTab=(RadioGroup)findViewById(R.id.main_tab);
+        mMainTab.setOnCheckedChangeListener(this);
+
+        mTabHost = getTabHost();
+
+        Intent iChart = new Intent(this, ChartActivity.class);
+        mTabHost.addTab(mTabHost.newTabSpec(TAB_TAG_CHART)
+                .setIndicator(getResources().getString(R.string.menu_chart), getResources().getDrawable(R.drawable.menu_chart_selector))
+                .setContent(iChart));
+
+        Intent iDatas = new Intent(this, DataActivity.class);
+        mTabHost.addTab(mTabHost.newTabSpec(TAB_TAG_DATAS)
+                .setIndicator(getResources().getString(R.string.menu_data), getResources().getDrawable(R.drawable.menu_data_selector))
+                .setContent(iDatas));
+
+        Intent iSquare = new Intent(this, SquareActivity.class);
+        mTabHost.addTab(mTabHost.newTabSpec(TAB_TAG_SQUARE)
+                .setIndicator(getResources().getString(R.string.menu_more), getResources().getDrawable(R.drawable.menu_more_selector))
+                .setContent(iSquare));
+
+        Intent iMore = new Intent(this, MoreActivity.class);
+        mTabHost.addTab(mTabHost.newTabSpec(TAB_TAG_MORE)
+                .setIndicator(getResources().getString(R.string.menu_more), getResources().getDrawable(R.drawable.menu_more_selector))
+                .setContent(iMore));
     }
 
     @Override
@@ -33,5 +70,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId) {
+            case R.id.MenuButtonChart:
+                this.mTabHost.setCurrentTabByTag(TAB_TAG_CHART);
+                break;
+            case R.id.MenuButtonDatas:
+                this.mTabHost.setCurrentTabByTag(TAB_TAG_DATAS);
+                break;
+            case R.id.MenuButtonSquare:
+                this.mTabHost.setCurrentTabByTag(TAB_TAG_SQUARE);
+                break;
+            case R.id.MenuButtonMore:
+                this.mTabHost.setCurrentTabByTag(TAB_TAG_MORE);
+                break;
+            default:
+                break;
+        }
     }
 }
